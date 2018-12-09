173a5f457ded91f0c2dc27486281348fb0792feb
krzysztofhejna <khejna15@wp.pl>
2018-12-09 11:48:16 +0100

    Add slider feature

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use-strict';
 (function () {
     var carousel = document.querySelector('.main-carousel');
+    var progressBar = document.querySelector('.carousel-progress-bar')
     var flkty = new Flickity(carousel, {cellALign: 'left', contain: true, hash: true});
-    flkty(carousel);
+    flkty.on('scroll', function(progress){
+        progress = Math.max( 0, Math.min( 1, progress));
+        progressBar.style.width = progress * 100 + '%';
+    });
 })();
